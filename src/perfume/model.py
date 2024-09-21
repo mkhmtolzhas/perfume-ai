6337552f78bbbcdd038bd76e25ce541f5145af63
Olzhas Makhmet <mkhmtcore@gmail.com>
2024-09-21 22:03:56 +0500

    chamged model

--- src/perfume/model.py
+++ src/perfume/model.py
@@ -6,17 +6,21 @@ class PerfumeInDB(BaseModel):
     name: str
     brand: str
     type: str
-    price: dict
-    description: list
+    price: int
+    description: str
+    attributes: list
     image: list
+    url: str
 
 
     def to_bson(self):
         return {
-            "name": self.name,
-            "brand": self.brand,
-            "type": self.type,
-            "price": self.price,
-            "description": self.description,
-            "image": self.image
+            'name': self.name,
+            'brand': self.brand,
+            'type': self.type,
+            'price': self.price,
+            'description': self.description,
+            'attributes': self.attributes,
+            'image': self.image,
+            'url': self.url
         }
